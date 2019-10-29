6b75a54957114867d4f8106d93ced2a8d9e88f86
Tarun M <tarunspartan1234@outlook.com>
2019-10-29 17:19:38 +0530

    pwa

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,11 +1,8 @@
-// This is the "Offline page" service worker
 
 const CACHE = "pwabuilder-page";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "index.html";
 
-// Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   console.log("[PWA Builder] Install Event processing");
 
